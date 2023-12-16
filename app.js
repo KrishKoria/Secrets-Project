7a3ec045bb815f4cbf92f1cb81d3e8add50b7484
KrishKoria <krishkoria2004@gmail.com>
2023-12-16 21:46:19 +0530

    added Secrets Project with level-2(encryption)

---
+++ app.js
@@ -0,0 +1,70 @@
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import mongooseEncryption from "mongoose-encryption";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = 3000;
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://localhost:27017/secretsDB");
+
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+userSchema.plugin(mongooseEncryption, {secret:process.env.SECRET_KEY, encryptedFields: ["password"]});
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+  res.render("home.ejs");
+});
+app.get("/login", (req, res) => {
+  res.render("login.ejs");
+});
+app.get("/register", (req, res) => {
+  res.render("register.ejs");
+});
+
+app.post("/register", async (req, res) => {
+  try {
+    const email = req.body.username
+    const password = req.body.password
+    const newUser = new User({
+      email: email,
+      password: password
+    });
+    await newUser.save();
+    res.render("secrets.ejs");
+  } catch (err) {
+    console.log(err);
+  }
+});
+app.post("/login", async (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  try {
+    const data = await User.findOne({email: username, password: password});
+    console.log(data)
+    if(data) {
+      if(data.password === password) {
+        res.render("secrets.ejs");
+      } else {
+        res.send("Wrong password");
+      }
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
